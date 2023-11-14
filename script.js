9ac585458d4771a80adc0623da53dbe93ee4df92
Ashwin3207 <58184287+Ashwin3207@users.noreply.github.com>
2023-11-14 23:30:11 +0530

    cahnges

---
+++ script.js
@@ -0,0 +1,29 @@
+let count=1;
+if(count==1)
+document.getElementById("poster").innerHTML='<img src="car1.jpg" width="100%">';
+if(count==2)
+document.getElementById("poster").innerHTML='<img src="car2.jpg" width="100%">';
+if(count==3)
+document.getElementById("poster").innerHTML='<img src="car3.jpg" width="100%">';
+if(count==4)
+document.getElementById("poster").innerHTML='<img src="car4.jpg" width="100%">';
+if(count==5)
+document.getElementById("poster").innerHTML='<img src="car5.jpg" width="100%">';
+if(count==6)
+document.getElementById("poster").innerHTML='<img src="car6.jpg" width="100%">';
+if(count==7)
+document.getElementById("poster").innerHTML='<img src="car7.jpg" width="100%">';
+function left()
+{
+if(count>1)
+count=count-1;
+
+console.log("left");
+
+}
+function right()
+{
+if(count<7)
+count=count+1;
+}
+console.log(count);
